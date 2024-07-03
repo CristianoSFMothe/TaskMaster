ad4088b1a4ec0c70fc451370c055b8a1f717acef
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-07-02 21:36:00 -0300

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,23 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions = {
+const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.JWT_SECRET as string,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account?.provider === 'google') {
+        // You can also check other account or profile properties
+        return true;
+      }
+      return false;
+    },
+  },
 };
 
-export default NextAuth(authOptions);
+export default NextAuth(options);
